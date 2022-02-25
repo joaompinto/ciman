43df44394ebf444b4dffa0e41bd83dc604174521
João Pinto <lamego.pinto@gmail.com>
2022-02-25 19:38:36 +0100

    Fixed flake8 warnings

--- ciman/__main__.py
+++ ciman/__main__.py
@@ -1,5 +1,28 @@
+import typer
+from .registry import DockerRegistryClient
+
+app = typer.Typer()
+
+
+@app.command()
+def info(image_name: str = typer.Argument(..., help="The name of the image")):
+    """
+    Show information for an image stored in a docker registry
+    """
+    drc = DockerRegistryClient()
+    drc.GetRepositoryInfo(image_name)
+
+
+@app.command()
+def pull(name: str = typer.Argument(..., help="The name of the user to greet")):
+    """
+    Pull image from a docker registry into a local directory
+    """
+    typer.echo("Not implemented yet")
+
+
 def main():
-    print("Hello")
+    app()
 
 
 if __name__ == "__main__":
