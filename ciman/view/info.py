4a5e8dc1eba4a272ba2048e9a473750114596475
João Pinto <lamego.pinto@gmail.com>
2022-02-28 18:22:49 +0100

    Strip default empty values

--- ciman/view/info.py
+++ ciman/view/info.py
@@ -1,30 +1,54 @@
 from rich.console import Console
 from rich.text import Text
+from ..jsonrules import print_item
+import json
 
 console = Console()
 
 
-def print_key(key, value):
+def print_text(text):
+    console.print(text)
+
+
+def print_key(key, _):
     print_key_and_value(key, "")
 
 
 def print_key_and_value(key, value):
-    text = Text.assemble((f"{key[0] : <16}", "bold magenta"), str(value))
-    console.print(text)
+    if isinstance(value, int):
+        value = str(value)
+    print_text(Text.assemble((f"{key[0] : <16}", "bold magenta"), value))
 
 
 def print_checksum(key, value):
     sum_type, sum_val = value.split(":")
-    text = Text.assemble(
-        16 * " ", str(key[-2]), ": ", (sum_type, "bold white"), ":", sum_val
+    print_text(
+        Text.assemble(
+            4 * " ", str(key[-2]), ": ", (sum_type, "bold white"), ":", sum_val
+        )
     )
-    console.print(text)
 
 
-def pretty_print(key, value):
-    # text = Text.assemble((f'{key[0] : <16}', "bold magenta"), str(key))
-    # console.print(text)
-    console.print_json(value)
+def delete_values(json_data, values_to_remove):
+    if isinstance(json_data, dict):
+        keys_to_delete = []
+        for key in json_data.keys():
+            value = json_data[key]
+            if value in values_to_remove:
+                keys_to_delete.append(key)
+            else:
+                delete_values(value, values_to_remove)
+        for key in keys_to_delete:
+            del json_data[key]
+    if isinstance(json_data, list):
+        for value in json_data:
+            delete_values(value, values_to_remove)
+
+
+def pretty_print(_, value):
+    value = json.loads(value)
+    delete_values(value, [False, "", None, {}])
+    console.print_json(json.dumps(value), skip_keys=False)
 
 
 json_print_rules = {
@@ -34,42 +58,6 @@ json_print_rules = {
     "blobSum": print_checksum,
     "v1Compatibility": pretty_print,
 }
-
-
-def check_name_rules(json_rules, key, value):
-    """ Check key name rules """
-    for rule_key, rule_func in json_rules.items():
-        if isinstance(rule_key, str) and rule_key.count(".") == 0:
-            if key and key[-1] == rule_key:
-                # print("MATCH", rule_key)
-                rule_func(key, value)
-
-
-def check_level_rules(json_rules, key, value):
-    level = len(key)
-    # Check rule levels only for leafs
-    for rule_key, rule_func in json_rules.items():
-        if isinstance(rule_key, int):
-            if level == rule_key:
-                rule_func(key, value)
-
-
-def print_item(json_rules, key, value):
-
-    # print("KVL", key, value, level)
-    check_name_rules(json_rules, key, value)
-
-    if isinstance(value, list):
-        for i, v in enumerate(value):
-            print_item(json_rules, key + [i], v)
-        return
-
-    if isinstance(value, dict):
-        for k, v in value.items():
-            print_item(json_rules, key + [k], v)
-        return
-
-    check_level_rules(json_rules, key, value)
 
 
 def print_image_info(image_json):
